8f963c7183117cd073f94564b8d0257b3292f134
Arek Cichocki <arexspeed95@gmail.com>
2021-01-08 17:41:54 +0100

    parsing payload

--- restful/index.js
+++ restful/index.js
@@ -3,6 +3,7 @@
 // Dependencies
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 // The server respond to all request with a string
 const server = http.createServer((req,res) =>{
@@ -22,12 +23,23 @@ const server = http.createServer((req,res) =>{
   //Get the headers as an object
   const headers = req.headers;
 
-  // Send response
-  res.end('Hello World\n')
-  // Log the request path -> to start in terminal curl localhost:5000/foo
-  console.log(parsedUrl)
-  console.log(`Request received on path ${trimedPath} with method: ${method} and with query string param`,queryStringObject )
-  console.log(`Req receives with these headers:`, headers)
+  //Get the payload
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+  // req.on -> request onEvent
+  req.on('data', (data) => {
+    buffer += decoder.write(data)
+  })
+  req.on('end', () => {
+    buffer += decoder.end();
+
+      // Send response
+      res.end('Hello World\n')
+      // Log the request path -> to start in terminal curl localhost:5000/foo
+    console.log(`Req receives with these payload:`, buffer) //to read post body in postman
+  })
+
+
 })
 
 //Start the server, and have it listen on port 5000
