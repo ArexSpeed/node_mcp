7054154f3d73b2af7a48f3225f19006e6deb681a
Arek Cichocki <arexspeed95@gmail.com>
2021-01-08 17:33:32 +0100

    parsing http,query,headers

--- restful/index.js
+++ restful/index.js
@@ -13,10 +13,21 @@ const server = http.createServer((req,res) =>{
   const path = parsedUrl.pathname
   const trimedPath = path.replace(/^\/+|\/+$/g, '')
 
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query
+
+  // get the HTTP Method
+  const method = req.method.toLowerCase();
+
+  //Get the headers as an object
+  const headers = req.headers;
+
   // Send response
   res.end('Hello World\n')
   // Log the request path -> to start in terminal curl localhost:5000/foo
-  console.log(`Request received on path ${trimedPath}` )
+  console.log(parsedUrl)
+  console.log(`Request received on path ${trimedPath} with method: ${method} and with query string param`,queryStringObject )
+  console.log(`Req receives with these headers:`, headers)
 })
 
 //Start the server, and have it listen on port 5000
