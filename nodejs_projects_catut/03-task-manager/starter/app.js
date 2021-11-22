48db58614a635b27ad153edb70ade73f4558f1f7
Arek Cichocki <arexspeed95@gmail.com>
2021-11-22 15:47:26 +0100

    setup routes, connect

--- nodejs_projects_catut/03-task-manager/starter/app.js
+++ nodejs_projects_catut/03-task-manager/starter/app.js
@@ -1,1 +1,30 @@
-console.log('Task Manager App')
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectBD =  require('./db/connect');
+
+dotenv.config();
+// middleware
+app.use(express.json())
+
+//routes
+app.get('/hello', (req,res) => {
+  res.send('Task Manager App');
+})
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 5000;
+
+const start = async () => {
+  try {
+    await connectBD(process.env.MONGO_URI)
+    app.listen(port, console.log(`server is listening on port ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start();
+
