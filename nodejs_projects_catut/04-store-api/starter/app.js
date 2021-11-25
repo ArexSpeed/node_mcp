04e99929ea3d3f4475abc07a8c3db6a1bfac1608
Arek Cichocki <arexspeed95@gmail.com>
2021-11-25 16:35:13 +0100

    setup & model

--- nodejs_projects_catut/04-store-api/starter/app.js
+++ nodejs_projects_catut/04-store-api/starter/app.js
@@ -1,1 +1,38 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors');
+//async errors
+
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
+const notFound = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products</a>')
+})
+
+app.use('/api/v1/products', productsRouter);
+
+//product route
+app.use(notFound);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 5000
+
+const start = async() => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening to the port ${port}`))
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
