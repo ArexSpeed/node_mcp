d5bc00507532a5e35986bae763b0cbf4f276d464
Arek <arexspeed95@gmail.com>
2022-05-10 15:14:01 +0200

    add db, create new Workout add to json file

--- crossfit-wod-api/src/index.js
+++ crossfit-wod-api/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express"); 
+const bodyParser = require("body-parser");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express(); 
@@ -9,6 +10,7 @@ app.get("/", (req, res) => {
     res.send("<h2>It's Working!</h2>"); 
 }); 
 
+app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => { 
